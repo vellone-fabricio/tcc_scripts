de44ede748a88eba00d89a101121a1330be7761b
vellone-fabricio <fabriciovellone@gmail.com>
2022-08-14 21:54:16 -0300

    adding create_graph.py

--- create_graphic.py
+++ create_graphic.py
@@ -1,8 +1,57 @@
 import matplotlib.pyplot as plt
+import csv
 
-plt.plot([0, 1, 2, 3, 4], [0, 3, 5, 9, 11])
-plt.xlabel('Months')
-plt.ylabel('Books Read')
+x_yarn = []
+y_yarn = []
+
+x_build = []
+y_build = []
+
+x_unit = []
+y_unit = []
+
+x_int = []
+y_int = []
+step_name=["yarn", "yarn build", "yarn test:unit", "yarn test:integration"]
+
+with open("filtered.csv", "r") as csv_file:
+  plots = csv.reader(csv_file, delimiter=",")
+
+  for row in plots:
+    if "attemp" in row:
+      continue
+
+    match row[1]:
+      case "yarn":
+        x_yarn.append(int(row[0]))
+        y_yarn.append(float(row[2]))
+      case "yarn build":
+        x_build.append(int(row[0]))
+        y_build.append(float(row[2]))
+      case "yarn test:unit":
+        x_unit.append(int(row[0]))
+        y_unit.append(float(row[2]))
+      case "yarn test:integration":
+        x_int.append(int(row[0]))
+        y_int.append(float(row[2]))
+
+
+for step in step_name:
+  match step:
+    case "yarn":
+      plt.plot(x_yarn, y_yarn, marker="*", label=step)
+    case "yarn build":
+      plt.plot(x_build, y_build, marker="*", label=step)
+    case "yarn test:unit":
+      plt.plot(x_unit, y_unit, marker="*", label=step)
+    case "yarn test:integration":
+      plt.plot(x_int, y_int, marker="*", label=step)
+
+
+plt.xlabel('Attempt')
+plt.ylabel('Consumption (J)')
+plt.title("Consumption per attemp")
+plt.legend()
 
 plt.show()
-plt.savefig('books_read.png')
+plt.savefig('consumption_output.png')
--- perf_script.sh
+++ perf_script.sh
@@ -8,9 +8,10 @@ step_name=("yarn"  "yarn build"  "yarn test:unit"  "yarn test:integration")
 echo "Creating .csv file to have all data..."
 echo "attemp,step,consumption(J),time(s)" > filtered.csv
 echo "File created!"
+echo
 
 
-for run in 1; do
+for run in 1 2 3 4 5 6 7 8 9 10; do
   echo "Run number: #${run} ---------------"
   for step in "${step_name[@]}"; do
     echo "Step name: '${step}'"
@@ -22,9 +23,15 @@ for run in 1; do
     echo "Filter file with Python..."
     python perfScript.py "${run}" "${step}" >> filtered.csv
     echo "Save in .csv!...\n"
+    echo
   done
 
   echo "Attempt done\n\n\n"
+  echo
+  echo
 done
+
+echo "Creating graph.."
+python create_graphic.py
 
 echo "Finished!"

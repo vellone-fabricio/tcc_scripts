86243739ba051b8fa4c604f4cf2d2d3ee39d612c
vellone-fabricio <fabriciovellone@gmail.com>
2022-08-16 22:54:27 -0300

    refactoring create_graph

--- create_graphic.py
+++ create_graphic.py
@@ -2,53 +2,41 @@ import matplotlib.pyplot as plt
 import csv
 import sys
 
+class StepInfos:
+  def __init__(self, step_name) -> None:
+    self.name = step_name
+    self.x = []
+    self.y = []
+
+  def add_x(self, value):
+    self.x.append(value)
+
+  def add_y(self, value):
+    self.y.append(value)
+
+
+# Configs =================
 PROJECT_NAME = sys.argv[1]
-
-x_yarn = []
-y_yarn = []
-
-x_build = []
-y_build = []
-
-x_unit = []
-y_unit = []
-
-x_int = []
-y_int = []
-step_name=["yarn", "yarn build", "yarn test:unit", "yarn test:integration"]
-
-with open("filtered.csv", "r") as csv_file:
-  plots = csv.reader(csv_file, delimiter=",")
-
-  for row in plots:
-    if "attempt" in row:
-      continue
-
-    match row[1]:
-      case "yarn":
-        x_yarn.append(int(row[0]))
-        y_yarn.append(float(row[2]))
-      case "yarn build":
-        x_build.append(int(row[0]))
-        y_build.append(float(row[2]))
-      case "yarn test:unit":
-        x_unit.append(int(row[0]))
-        y_unit.append(float(row[2]))
-      case "yarn test:integration":
-        x_int.append(int(row[0]))
-        y_int.append(float(row[2]))
-
+step_name=["yarn", "yarn build", "yarn test", "yarn test:docker:up", "yarn test:integration", "yarn test:docker:down"]
+# =========================
 
 for step in step_name:
-  match step:
-    case "yarn":
-      plt.plot(x_yarn, y_yarn, marker="*", label=step)
-    case "yarn build":
-      plt.plot(x_build, y_build, marker="*", label=step)
-    case "yarn test:unit":
-      plt.plot(x_unit, y_unit, marker="*", label=step)
-    case "yarn test:integration":
-      plt.plot(x_int, y_int, marker="*", label=step)
+  my_step = StepInfos(step)
+
+  with open("filtered.csv", "r") as csv_file:
+    plots = csv.reader(csv_file, delimiter=",")
+
+    for row in plots:
+      if "attempt" in row:
+        continue
+
+      if my_step.name == row[1]:
+        my_step.add_x(int(row[0]))
+        my_step.add_y(float(row[2]))
+
+
+
+  plt.plot(my_step.x, my_step.y, marker="*", label=my_step.name)
 
 
 plt.xlabel('Attempt')
@@ -56,5 +44,4 @@ plt.ylabel('Consumption (J)')
 plt.title(f"Consumption per attempt - {PROJECT_NAME}")
 plt.legend()
 
-plt.show()
 plt.savefig('consumption_output.png')
